29d042046d6b09ba26eeb9fa50bfe61f029f683b
SeneSatka <senesatka@gmail.com>
2025-01-27 20:48:46 +0300

    feat: add Sidebar component and new views for ElementDetail and NotFound

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Sidebar from "./components/Sidebar.vue";
 </script>
 <template>
-  <div class="bg-default w-full min-h-screen text-white"><RouterView /></div>
+  <div class="bg-default w-full min-h-screen text-white">
+    <RouterView /> <Sidebar />
+  </div>
 </template>
